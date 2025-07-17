042fccce0d349d92390ef2806c0c45625b92c215
Daniel Borrome <dborrome@gmail.com>
2025-07-17 11:13:28 -0400

    Agregar ApplicationDbContext y configurar la conexión a la base de datos

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var dbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConnectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
