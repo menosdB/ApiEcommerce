87125dac066417cabbe43ab8f3856b837b58efb2
Daniel Borrome <dborrome@gmail.com>
2025-08-16 13:11:54 -0400

    Agregar modelo de Product y migraciones para la creación de la tabla Products

--- ApiEcommerce/Data/ApplicationDbContext.cs
+++ ApiEcommerce/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using ApiEcommerce.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : DbContext
@@ -9,4 +10,5 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
 }
